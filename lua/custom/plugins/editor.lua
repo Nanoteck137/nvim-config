b2977431d9169aafaf540e5e068cf72b379a777f
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-11-24 16:29:26 +0100

    Added trouble

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -1,13 +1,37 @@
 return {
-  {
-    "dstein64/vim-startuptime",
-    cmd = "StartupTime",
-    config = function()
-      vim.g.startuptime_tries = 10
-    end,
-  },
-
   'tpope/vim-sleuth',
 
-  { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'numToStr/Comment.nvim',
+
+    keys = {
+      'gcc',
+      'gbc',
+      { 'gc', mode = 'v' },
+      { 'gb', mode = 'v' },
+    },
+
+    opts = {},
+  },
+
+  {
+    -- More options: https://github.com/folke/trouble.nvim
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+
+    cmd = { 'Trouble', 'TroubleClose', 'TroubleToggle', 'TroubleRefresh' },
+
+    keys = {
+      { '<leader>xx', function() require('trouble').toggle() end,                        desc = 'Toggle Trouble' },
+      { '<leader>xw', function() require('trouble').toggle('workspace_diagnostics') end, desc = 'Toggle Trouble (Worksapce)' },
+      { '<leader>xd', function() require('trouble').toggle('document_diagnostics') end,  desc = 'Toggle Trouble (Document)' },
+      { '<leader>xq', function() require('trouble').toggle('quickfix') end,              desc = 'Toggle Trouble (Quickfix)' },
+      { '<leader>xl', function() require('trouble').toggle('loclist') end,               desc = 'Toggle Trouble (Loclist)' },
+      { '<leader>xr', function() require('trouble').refresh() end,                       desc = 'Refresh Trouble' },
+    },
+
+    opts = {
+    },
+
+  }
 }
